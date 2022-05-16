5a63bdabcff0888b962389efcca37b78eac20b21
Kay941997 <Kay941997@gmail.com>
2022-05-16 10:32:12 +0700

    Add Middleware

---
+++ src/middlewares/audit.middleware.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+import { Injectable, NestMiddleware } from "@nestjs/common";
+
+//!Middleware:
+@Injectable()
+export class AuditMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: Function) {
+        console.log("Logging DELETE request Headers ", req.headers);
+        next()
+    }
+
+ 
+    
+
+}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { CacheModule, CACHE_MANAGER, Module } from '@nestjs/common';
+import { CacheModule, CACHE_MANAGER, MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,6 +7,7 @@ import { TaskRepository } from './task.repository';
 import { AuthModule } from 'src/auth/auth.module';
 import { CaslModule } from 'src/casl/casl.module';
 import { RedisCacheModule } from 'src/cacheRedis/redis.module';
+import { AuditMiddleware } from 'src/middlewares/audit.middleware';
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { RedisCacheModule } from 'src/cacheRedis/redis.module';
 
     CaslModule, //!CASL Role
 
-    RedisCacheModule, //!Redis Cache: (+ Docker)
+    RedisCacheModule, //!Redis Cache
     CacheModule.register({ //!In-memory Cache | Cache Manually:
       ttl: 10, //thời gian hết hạn của bộ nhớ Cache, sau khi xóa sẽ cập nhật danh sách sau 10s
       max: 100, //maximum number of items in Cache
@@ -24,4 +25,11 @@ import { RedisCacheModule } from 'src/cacheRedis/redis.module';
   providers: [TasksService],
   controllers: [TasksController],
 })
-export class TasksModule {}
+export class TasksModule implements NestModule { //!Middleware
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuditMiddleware)
+      .forRoutes({ path: 'tasks/*', method: RequestMethod.DELETE }) //todo: task.controller Delete
+  } 
+  
+}
